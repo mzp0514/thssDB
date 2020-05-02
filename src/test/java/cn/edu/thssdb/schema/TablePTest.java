9494fdcc571999c12a094ef2f7aa3a1883d5df7f
RUBBY <hgd17@mails.tsinghua.edu.cn>
2020-05-02 13:20:48 +0800

    update TablePTest

--- src/test/java/cn/edu/thssdb/schema/TablePTest.java
+++ src/test/java/cn/edu/thssdb/schema/TablePTest.java
@@ -18,32 +18,21 @@ public class TablePTest {
 
 	private TableP table;
 
+	Column[] columns;
+
 	private ArrayList<Row> rows;
 
 	@Before
 	public void setUp() throws IOException {
-		Column[] columns = new Column[5];
+		columns = new Column[5];
 //		String name, ColumnType type, int primary, boolean notNull, int maxLength
 		columns[0] = new Column("id", ColumnType.INT, 1, true , 0);
 		columns[1] = new Column("name", ColumnType.STRING, 0, true , 3);
 		columns[2] = new Column("class", ColumnType.STRING, 0, true , 2);
 		columns[3] = new Column("height", ColumnType.DOUBLE, 0, true , 0);
 		columns[4] = new Column("weight", ColumnType.DOUBLE, 0, true , 0);
-	//	this.table = new TableP("dbtest", "tb", columns);
 
 		this.rows = new ArrayList<>();
-	}
-
-	@Test
-	public void resume() throws IOException, ClassNotFoundException {
-		insertTest();
-		TableP table2 = new TableP("dbtest", "tb");
-		//assertEquals(table.index.size(),table2.index.size());
-	}
-
-
-	@Test
-	public void insertTest() throws IOException {
 		Entry[] entries = new Entry[5];
 		entries[0] = new Entry(0);
 		entries[1] = new Entry("mzp");
@@ -54,35 +43,30 @@ public class TablePTest {
 			entries[3] = new Entry((double)i);
 			entries[4] = new Entry((double)i);
 			Row row = new Row(entries);
-			//table.insert(row);
 			rows.add(row);
 		}
-
 	}
 
 	@Test
-	public void deleteTest() throws IOException, ClassNotFoundException {
-		insertTest();
+	public void insertTest() throws IOException {
+		this.table = new TableP("dbtest", "tb", columns);
 		Entry[] entries = new Entry[5];
 		entries[0] = new Entry(0);
 		entries[1] = new Entry("mzp");
 		entries[2] = new Entry("73");
-		ArrayList<Row> rows = new ArrayList<>();
-		for(int i = 0; i < 200; i++) {
+
+		for(int i = 0; i < 1000; i++){
 			entries[0] = new Entry(i);
-			entries[3] = new Entry((double) i);
-			entries[4] = new Entry((double) i);
+			entries[3] = new Entry((double)i);
+			entries[4] = new Entry((double)i);
 			Row row = new Row(entries);
-			rows.add(row);
+			table.insert(row);
 		}
 
-		table.delete(rows);
-		//assertEquals(800, table.index.size());
 	}
 
 	@Test
 	public void selectTest() throws IOException, ClassNotFoundException {
-		insertTest();
 		TableP table2 = new TableP("dbtest", "tb");
 		ArrayList<Row> res1 = table2.select("id", 100, ComparisonType.EQUAL);
 
@@ -97,9 +81,9 @@ public class TablePTest {
 	}
 
 	@Test
-	public void selectTest2() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("id", 100, ComparisonType.NEQUAL);
+	public void selectTest2() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("id", 100, ComparisonType.NEQUAL);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -113,7 +97,6 @@ public class TablePTest {
 
 	@Test
 	public void selectTest3() throws IOException, ClassNotFoundException {
-		insertTest();
 		TableP table2 = new TableP("dbtest", "tb");
 		ArrayList<Row> res1 = table2.select("id", 200, ComparisonType.GREATER);
 
@@ -128,9 +111,9 @@ public class TablePTest {
 	}
 
 	@Test
-	public void selectTest4() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("id", 50, ComparisonType.LESS);
+	public void selectTest4() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("id", 50, ComparisonType.LESS);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -143,9 +126,9 @@ public class TablePTest {
 	}
 
 	@Test
-	public void selectTest5() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("id", 50, ComparisonType.NLESS);
+	public void selectTest5() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("id", 50, ComparisonType.NLESS);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -158,9 +141,9 @@ public class TablePTest {
 	}
 
 	@Test
-	public void selectTest6() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("id", 50, ComparisonType.NGREATER);
+	public void selectTest6() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("id", 50, ComparisonType.NGREATER);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -173,9 +156,9 @@ public class TablePTest {
 	}
 
 	@Test
-	public void selectTest7() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("name", "mzpp", ComparisonType.NGREATER);
+	public void selectTest7() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("name", "mzpp", ComparisonType.NGREATER);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -188,9 +171,9 @@ public class TablePTest {
 	}
 
 	@Test
-	public void selectTest8() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("height", 100.0, ComparisonType.NGREATER);
+	public void selectTest8() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("height", 100.0, ComparisonType.NGREATER);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -203,9 +186,9 @@ public class TablePTest {
 	}
 
 	@Test
-	public void selectTest9() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("height", 100.0, ComparisonType.EQUAL);
+	public void selectTest9() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("height", 100.0, ComparisonType.EQUAL);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -214,13 +197,13 @@ public class TablePTest {
 			}
 		}
 
-		assertEquals(res2, res1);
+		assertEquals(res2.toString(), res1.toString());
 	}
 
 	@Test
-	public void selectTest10() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("height", 100.0, ComparisonType.NEQUAL);
+	public void selectTest10() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("height", 100.0, ComparisonType.NEQUAL);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -229,13 +212,13 @@ public class TablePTest {
 			}
 		}
 
-		assertEquals(res2, res1);
+		assertEquals(res2.toString(), res1.toString());
 	}
 
 	@Test
-	public void selectTest11() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("height", 100.0, ComparisonType.GREATER);
+	public void selectTest11() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("height", 100.0, ComparisonType.GREATER);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -244,13 +227,13 @@ public class TablePTest {
 			}
 		}
 
-		assertEquals(res2, res1);
+		assertEquals(res2.toString(), res1.toString());
 	}
 
 	@Test
-	public void selectTest12() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("height", 100.0, ComparisonType.LESS);
+	public void selectTest12() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("height", 100.0, ComparisonType.LESS);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -259,13 +242,13 @@ public class TablePTest {
 			}
 		}
 
-		assertEquals(res2, res1);
+		assertEquals(res2.toString(), res1.toString());
 	}
 
 	@Test
-	public void selectTest13() throws IOException {
-		insertTest();
-		ArrayList<Row> res1 = table.select("height", 100.0, ComparisonType.NLESS);
+	public void selectTest13() throws IOException, ClassNotFoundException {
+		TableP table2 = new TableP("dbtest", "tb");
+		ArrayList<Row> res1 = table2.select("height", 100.0, ComparisonType.NLESS);
 
 		ArrayList<Row> res2 = new ArrayList<>();
 		for(Row r: rows){
@@ -274,6 +257,6 @@ public class TablePTest {
 			}
 		}
 
-		assertEquals(res2, res1);
+		assertEquals(res2.toString(), res1.toString());
 	}
 }
